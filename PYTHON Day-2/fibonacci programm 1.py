656c0d75e2db418863a47383084e9da3009d61e2
shaikfazlu <121933439+shaikfazlu@users.noreply.github.com>
2023-02-09 14:41:00 +0530

    Add files via upload

---
+++ PYTHON Day-2/fibonacci programm 1.py
@@ -0,0 +1,12 @@
+def fibonacci(n1):
+    if n1 == 0:
+        return 0
+    elif n1 == 1:
+        return 1
+    else:
+        return fibonacci(n1-1) + fibonacci(n1-2)
+
+def waycount (n):
+    return fibonacci(n+1)
+n=2
+print("The number of ways to reach the top of a staircase with", n, "steps is", waycount(n))
